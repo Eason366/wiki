bc779da08f2b06357bd9b8c5aa0f72a71676fb11
Eason-Wangy366 <754459450@qq.com>
2022-03-06 18:40:49 -0500

    3-6 add a common return class CommonResp

--- src/main/java/com/eason/wiki/controller/EbookController.java
+++ src/main/java/com/eason/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.eason.wiki.controller;
 
 import com.eason.wiki.domain.Ebook;
+import com.eason.wiki.resp.CommonResp;
 import com.eason.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,12 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list =  ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
