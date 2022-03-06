6509524ac34f621a6fd1367c6f58525ec770acf4
Eason-Wangy366 <754459450@qq.com>
2022-03-06 03:42:56 -0500

    2-8 Add Custom configuration

--- src/main/java/com/eason/wiki/controller/TestController.java
+++ src/main/java/com/eason/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.eason.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${author:Test}")
+    private String author;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello "+ author;
     }
 
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "Hello World Post " + name;
     }
+
+
 }
