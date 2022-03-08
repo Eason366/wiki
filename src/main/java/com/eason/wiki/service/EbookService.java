d90ff3d18d10febcb7e83ec8bd0d342a20e89471
Eason-Wangy366 <754459450@qq.com>
2022-03-08 01:06:22 -0500

    6-4 add pagehelper

--- src/main/java/com/eason/wiki/service/EbookService.java
+++ src/main/java/com/eason/wiki/service/EbookService.java
@@ -7,6 +7,10 @@ import com.eason.wiki.mapper.EbookMapper;
 import com.eason.wiki.req.EbookReq;
 import com.eason.wiki.resp.EbookResp;
 import com.eason.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -18,6 +22,7 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
 
     @Resource
@@ -29,7 +34,12 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName()))
             criteria.andNameLike("%"+req.getName()+"%");
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Total length: {}",pageInfo.getTotal());
+        LOG.info("Total pages: {}",pageInfo.getPages());
+
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
         return  list;
     }
